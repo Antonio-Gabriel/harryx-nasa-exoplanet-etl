d7a1f640229bffa9477e8fc09490545ac02e1890
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-07-31 13:15:57 +0100

    configure database methods to persist and get planets

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,1 +1,16 @@
-// TODO: add sequalizer here to load data into the database system
+import { Planet } from 'src/model/planet'
+import { prismaClient } from './prisma-client'
+import { PlanetsModelMapper } from 'src/model/mappers/planets-mapper'
+
+export class Database {
+  public async create(planet: Planet) {
+    await prismaClient.planets.create({
+      data: PlanetsModelMapper.toModel(planet),
+    })
+  }
+
+  public async getPlanets(): Promise<Planet[]> {
+    const planets = await prismaClient.planets.findMany()
+    return planets.map(PlanetsModelMapper.toEntity)
+  }
+}
